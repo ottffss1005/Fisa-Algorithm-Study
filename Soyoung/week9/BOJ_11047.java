9e18b98f42e4abcbcd922bda9cb40a53017d2d3f
So Young Choi <62324475+ottffss1005@users.noreply.github.com>
2025-10-22 00:24:57 +0900

    feat(Soyoung): add /Baekjoon-11047.java

---
+++ Soyoung/week9/BOJ_11047.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int [] arr = new int [N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (arr[i] <= M) {
+                count += M / arr[i];
+                M = M % arr[i];
+            }
+        }
+
+        bw.write(count); 
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
